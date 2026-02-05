9655043858fbd7604e5119d5a7fcc257a0fc4fa6
AshwinSivanandam <46950563+AshwinSivanandam@users.noreply.github.com>
2026-02-05 22:11:29 +0530

    Updated bucket name in backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "devsecops-tetris-reyaz-111" # Replace with your actual S3 bucket name
+    bucket = "ashwin.28111994nov" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
+    region = "us-east-1"
   }
 }
